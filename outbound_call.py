012c97adcc28208f4ea199537e51cc2d0724a7fb
tobenik <tobias.nikko@gmail.com>
2025-03-02 09:50:18 +0200

    rename env variables.

--- outbound_call.py
+++ outbound_call.py
@@ -9,7 +9,7 @@ auth_token = os.getenv('AUTH_TOKEN')
 client = Client(account_sid, auth_token)
 
 def make_call(to_number):
-    phone_number = os.getenv('PHONE_NO')
+    phone_number = os.getenv('TWILIO_NUM')
     ngrok_url = os.getenv('NGROK_URL')
     webhook_url = f"{ngrok_url}/answer"
     
@@ -21,5 +21,5 @@ def make_call(to_number):
     print(f"Call SID: {call.sid}")
 
 if __name__ == "__main__":
-    recipient_no = os.getenv('RECIPIENT_NO')
+    recipient_no = os.getenv('RECIPIENT_NUM')
     make_call(recipient_no)
